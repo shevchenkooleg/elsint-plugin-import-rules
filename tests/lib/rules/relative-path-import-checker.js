6651385c7cd084add4424d781bbf7dca7ab79a75
Oleg <shevchenko.oleg.87@gmail.com>
2023-06-10 22:15:05 +0300

    add relative-path-import-checker autofix

--- tests/lib/rules/relative-path-import-checker.js
+++ tests/lib/rules/relative-path-import-checker.js
@@ -33,13 +33,15 @@ ruleTester.run("relative-path-import-checker", rule, {
   invalid: [
     {
       filename: '/Users/oleg/programming_training/UlbiTV/my-blog-project/src/pages/ProfilePage/ui/ProfilePage.tsx',
-      code: "import { classNames } from 'pages/ProfilePage/ui/ProfilePage'",
+      code: "import { classNames } from 'pages/ProfilePage/ui/lib'",
       errors: [{ message: "Component imports within the same module should be relative"}],
+      output: "import { classNames } from './lib'",
     },
     {
-      filename: '/Users/oleg/programming_training/UlbiTV/my-blog-project/src/pages/ProfilePage/ui/ProfilePage.tsx',
-      code: "import { classNames } from '@/pages/ProfilePage/ui/ProfilePage'",
+      filename: '/Users/oleg/programming_training/UlbiTV/my-blog-project/src/pages/ProfilePage/model/Slice.ts',
+      code: "import { classNames } from '@/pages/ProfilePage'",
       errors: [{ message: "Component imports within the same module should be relative"}],
+      output: "import { classNames } from '..'",
       options: [{
         alias: '@'
       }]
